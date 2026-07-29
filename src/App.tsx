0561ca0d0ef6e40185f298fcfe6a813ea0bae438
wangduokaiwait-hash <wangduokaiwait-hash@users.noreply.github.com>
2026-07-29 17:51:07 +0800

    fix: 移除优惠券相关代码，修复 Vite 导入错误

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import { useState, useCallback } from 'react';
 import type { AddressItem as AddressItemType } from './types/address';
 import AddressList from './components/AddressList';
-import CouponModal from './components/CouponModal';
 import { mockAddresses } from './data/mockAddress';
-import { mockCoupons } from './data/mockCoupon';
 import styles from './App.module.css';
 
 const App = () => {
-  const [couponVisible, setCouponVisible] = useState(false);
-
   const handleEdit = (item: AddressItemType) => {
     // eslint-disable-next-line no-alert
     alert(`编辑地址: ${item.name} - ${item.address}`);
   };
-
-  const handleOpenCoupons = useCallback(() => {
-    setCouponVisible(true);
-  }, []);
-
-  const handleCloseCoupons = useCallback(() => {
-    setCouponVisible(false);
-  }, []);
-
-  const handleClaimCoupon = useCallback((id: number) => {
-    // eslint-disable-next-line no-console
-    console.log('[App] 领取优惠券:', id);
-  }, []);
 
   return (
     <div className={styles.page}>
@@ -34,24 +16,7 @@ const App = () => {
       </div>
       <main className={styles.main}>
         <AddressList initialData={mockAddresses} onEdit={handleEdit} />
-
-        {/* 优惠券入口 */}
-        <div className={styles.couponEntry}>
-          <button className={styles.couponBtn} onClick={handleOpenCoupons}>
-            <span className={styles.couponBtnIcon}>🎫</span>
-            <span className={styles.couponBtnText}>查看优惠券</span>
-            <span className={styles.couponBtnArrow}>›</span>
-          </button>
-        </div>
       </main>
-
-      {/* 优惠券浮层 */}
-      <CouponModal
-        visible={couponVisible}
-        coupons={mockCoupons}
-        onClose={handleCloseCoupons}
-        onClaim={handleClaimCoupon}
-      />
     </div>
   );
 };
